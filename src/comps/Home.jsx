e779d806be750446dbf758ef224fcf37387c648b
Adwait Samak <adwaitsamak77@gmail.com>
2024-04-04 09:49:43 +0530

    Repositioned UI and removed extra routes

--- src/comps/Home.jsx
+++ src/comps/Home.jsx
@@ -1,16 +1,36 @@
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { AgGridReact } from "ag-grid-react"; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function Home() {
   let gridApi = useRef(null);
+  const [selectedRows, setSelectedRows] = useState([]); // State to store selected rows
   const [showPreferences, setShowPreferences] = useState(false);
-  const [userpreferencemap, setUserpreferencemap] = useState({});
+  const [userpreferencemap, setUserpreferencemap] = useState({ select: true });
+  const [selectedrowid, setSelectedrowid] = useState([]);
+
+  useEffect(() => {
+    if (selectedRows) {
+      const arr = [];
+      selectedRows.forEach((val) => {
+        arr.push(val.id);
+      });
+      setSelectedrowid(arr);
+      // console.log(arr);
+    }
+  }, [selectedRows]);
 
   const colDefs = [
+    {
+      headerName: "Select",
+      headerCheckboxSelection: true,
+      checkboxSelection: true,
+      width: 150,
+    },
     { headerName: "Id", field: "id", width: 150 },
     { headerName: "Name", field: "name", width: 150 },
     { headerName: "Username", field: "username", width: 150 },
@@ -24,10 +44,9 @@ function Home() {
 
   const [data, setData] = useState([]);
   useEffect(() => {
-    const url = "http://localhost:3000/users";
     const fetchData = async () => {
       try {
-        const response = await axios.get(url);
+        const response = await axios.get("http://localhost:3000/users");
         setData(response.data);
       } catch (error) {
         console.log("Error in fetching data", error);
@@ -37,12 +56,10 @@ function Home() {
   }, []);
 
   useEffect(() => {
-    // jab component start hoga, apan local storage se userpreferencemap uthayenge
     const storedPreferences = localStorage.getItem("userpreferencemap");
     if (storedPreferences) {
       setUserpreferencemap(JSON.parse(storedPreferences));
     } else {
-      // initial case me agar userpreference map nahi mila to apan sare columns ko initially true set kr denge, so that all are visible
       const initialPreferences = {};
       colDefs.forEach((column) => {
         initialPreferences[column.field] = true;
@@ -65,16 +82,16 @@ function Home() {
     }
   }, [colDefs]);
 
-  // console.log(userpreferencemap);
-
-  const handlePreferences = (e) => {
-    e.preventDefault();
-    setShowPreferences((prev) => !prev);
-    if (!showPreferences) alert("Deselect the columns you want to hide");
+  const handlePreferences = () => {
+    if (!showPreferences) {
+      alert("Uncheck the boxes to hide columns");
+      setShowPreferences(true);
+    } else {
+      setShowPreferences(false);
+    }
   };
 
-  const savepreferences = (e) => {
-    e.preventDefault();
+  const savepreferences = () => {
     localStorage.setItem(
       "userpreferencemap",
       JSON.stringify(userpreferencemap)
@@ -83,68 +100,109 @@ function Home() {
 
   const handleOnChangeCheckbox = (field, checked) => {
     setUserpreferencemap((prevPreferences) => ({
-      ...prevPreferences, //apan spread kr rhe hai previous map ko and just changing the field changed field
+      ...prevPreferences,
       [field]: checked,
     }));
   };
 
+  const handleDelete = () => {
+    const confirm = window.confirm("Would you like to delete this record?");
+    const num = selectedrowid[0];
+    console.log(num);
+    console.log(typeof num);
+    if (confirm) {
+      axios
+        .delete(`http://localhost:3000/users/${selectedrowid[0]}`)
+        .then((res) => {
+          location.reload();
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   const visibleColDefs = colDefs.filter(
-    (column) => userpreferencemap[column.field]===true
+    (column) => userpreferencemap[column.field] === true
   );
 
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center bg-light m-4 p-2">
-      <div className="w-75 rounded bg-white border shadow p-4">
+    <div
+      className="d-flex flex-column justify-content-center align-items-center bg-light m-4 p-2"
+      style={{ height: "100vh" }}
+    >
+      <div
+        className="w-75 rounded bg-white border shadow p-4"
+        style={{ width: "90%" }}
+      >
+        <div className="d-flex gap-5">
+          <Dropdown className="">
+            <Dropdown.Toggle variant="primary" id="dropdown-basic-button">
+              Actions
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href={`/read/${selectedrowid[0]}`}>
+                Read
+              </Dropdown.Item>
+              <Dropdown.Item href={`/update/${selectedrowid[0]}`}>
+                Action
+              </Dropdown.Item>
+              <Dropdown.Item onClick={handleDelete}>Delete</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic-button">
+              Preferences
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={handlePreferences}>
+                Preferences
+              </Dropdown.Item>
+              <Dropdown.Item onClick={savepreferences}>Save</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <div className="d-flex gap-5 align-items-center justify-content-center p-3">
+            {showPreferences && (
+              <div className="d-flex gap-2">
+                {colDefs.map((column) => (
+                  <label key={column.headerName.toLowerCase()}>
+                    <input
+                      type="checkbox"
+                      checked={userpreferencemap[column.field]}
+                      disabled={column.headerName === "Select"}
+                      onChange={(e) =>
+                        handleOnChangeCheckbox(column.field, e.target.checked)
+                      }
+                    />
+                    {column.headerName}
+                  </label>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+
         <div className="table-responsive d-flex justify-content-end">
-          <Link to="/create" className="btn btn-success">
+          <Link to="/create" className="btn btn-success mb-3">
             Add User
           </Link>
         </div>
         <div
           className="ag-theme-quartz"
-          style={{ height: 302, width: 750, justifyContent: "center" }}
+          style={{ height: "58vh", width: "100%" }}
         >
           <AgGridReact
             rowData={data}
             columnDefs={visibleColDefs}
             onGridReady={onGridReady}
             suppressHorizontalScroll={true}
+            rowSelection="multiple"
+            onSelectionChanged={(event) => {
+              setSelectedRows(event.api.getSelectedRows());
+              // console.log(selectedRows);
+            }}
           />
         </div>
-      </div>
-      <div className="d-flex flex-row m-3 gap-5 p-3">
-        <Link to={`/getid`} className="btn btn-primary me-2">
-          Read
-        </Link>
-        <Link to={`/getidedit`} className="btn btn-primary me-2">
-          Edit
-        </Link>
-        <Link to={`/getiddelete`} className="btn btn-danger">
-          Delete
-        </Link>
-      </div>
-      <div className="d-flex gap-5 align-items-center justify-content-center">
-        <button onClick={handlePreferences} className="btn btn-primary">Preferences</button>
-        <div>
-          {showPreferences && (
-            <div className="d-flex gap-2">
-              {colDefs.map((column) => (
-                <label key={column.field}>
-                  <input
-                    type="checkbox"
-                    checked={userpreferencemap[column.field]}
-                    disabled={column.field === "srno"} //we dont want user to hide srno, we we disbale toggling checkbox for it
-                    onChange={(e) =>
-                      handleOnChangeCheckbox(column.field, e.target.checked)
-                    }
-                  />
-                  {column.headerName}
-                </label>
-              ))}
-            </div>
-          )}
-        </div>
-        <button onClick={savepreferences} className="btn btn-success">Save</button>
       </div>
     </div>
   );
