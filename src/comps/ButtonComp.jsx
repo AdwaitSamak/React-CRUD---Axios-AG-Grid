44c2ba760d6ddfc1c9febba8e2bb42b2322b6af5
Adwait Samak <adwaitsamak77@gmail.com>
2024-05-23 11:51:19 +0530

    Added Modal to delete record, added quick and text filters, changed CSS, fixed width issue, added coloured bands and other minor changes

--- src/comps/ButtonComp.jsx
+++ src/comps/ButtonComp.jsx
@@ -1,26 +1,35 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { CiEdit } from "react-icons/ci";
 import { MdDelete } from "react-icons/md";
 import Tooltip from "@mui/material/Tooltip";
+import { Dialog, DialogActions, DialogTitle } from "@mui/material";
 
 function ButtonComp(props) {
   // console.log(props);
   const rowid = props.rowid;
   console.log(rowid);
+
+  const [modalopen, setModalOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setModalOpen(true);
+  };
+
+  const handleClose = () => {
+    setModalOpen(false);
+  };
+
   const handledelete = (e) => {
     e.preventDefault();
     console.log(rowid);
-    const confirm = window.confirm("Would you like to delete this record?");
-    if (confirm) {
-      axios
-        .delete(`http://localhost:3000/users/${rowid}`)
-        .then((res) => {
-          location.reload();
-        })
-        .catch((error) => console.log(error));
-    }
+    axios
+      .delete(`http://localhost:3000/users/${rowid}`)
+      .then((res) => {
+        location.reload();
+      })
+      .catch((error) => console.log(error));
   };
   return (
     <div className="d-flex gap-2 justify-content-center">
@@ -30,10 +39,18 @@ function ButtonComp(props) {
         </Link>
       </Tooltip>
       <Tooltip title="Delete">
-        <button onClick={handledelete} className="btn">
+        <button onClick={handleClickOpen} className="btn">
           <MdDelete />
         </button>
       </Tooltip>
+
+      <Dialog open={modalopen} onClose={handleClose}>
+        <DialogTitle>{"Do you want to delete this record?"}</DialogTitle>
+        <DialogActions>
+          <button onClick={handleClose} className="px-3 border-1">No</button>
+          <button onClick={handledelete} className="px-3 border-1">Yes</button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
--- src/comps/Home.jsx
+++ src/comps/Home.jsx
@@ -1,15 +1,21 @@
 import axios from "axios";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { AgGridReact } from "ag-grid-react"; // AG Grid Component
+import "ag-grid-enterprise";
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css";
+import { SetFilterModule } from "@ag-grid-enterprise/set-filter";
 import Dropdown from "react-bootstrap/Dropdown";
 import ButtonComp from "./ButtonComp";
 import { SlOptionsVertical } from "react-icons/sl";
 import { IoMdAdd } from "react-icons/io";
 import { Tooltip } from "react-tooltip";
-import { BiSolidHide } from "react-icons/bi";
+import { ModuleRegistry } from "@ag-grid-community/core";
+import "./styles.css";
+import { Drawer } from "@mui/material";
+
+ModuleRegistry.registerModules([SetFilterModule]);
 
 function Home() {
   let gridApi = useRef(null);
@@ -19,6 +25,15 @@ function Home() {
   const [selectedrowid, setSelectedrowid] = useState(null);
   const [data, setData] = useState([]);
   const [isrowselected, setisrowselected] = useState(false);
+  const [preferencedrawer, setPreferencedrawer] = useState(false);
+
+  const NameFilterParams = {
+    // filterOptions: ["contains"],
+    trimInput: true,
+    caseSensitive: true,
+    buttons: ["apply", "reset", "clear"],
+    closeOnApply: true,
+  };
 
   useEffect(() => {
     if (selectedRows && selectedRows.length > 0) {
@@ -30,16 +45,90 @@ function Home() {
   }, [selectedRows]);
 
   const colDefs = [
-    { headerName: "Id", field: "id", width: 150 },
-    { headerName: "Name", field: "name", width: 150 },
-    { headerName: "Username", field: "username", width: 150 },
-    { headerName: "Email", field: "email", width: 150 },
-    { headerName: "Phone", field: "phone", width: 150 },
+    {
+      headerName: "Id",
+      field: "id",
+      width: 150,
+      filter: "agTextColumnFilter",
+      filterParams: {
+        filterOptions: ["equals"],
+        buttons: ["apply", "reset", "clear"],
+        closeOnApply: true,
+      },
+      floatingFilter: true,
+      suppressHeaderMenuButton: true,
+      unSortIcon: true,
+    },
+    {
+      headerName: "Name",
+      field: "name",
+      width: 150,
+      filter: "agTextColumnFilter",
+      filterParams: NameFilterParams,
+      floatingFilter: true,
+      suppressHeaderMenuButton: true,
+      unSortIcon: true,
+    },
+    {
+      headerName: "Username",
+      field: "username",
+      width: 150,
+      filter: "agTextColumnFilter",
+      filterParams: {
+        filterOptions: ["contains"],
+        buttons: ["apply", "reset", "clear"],
+        closeOnApply: true,
+      },
+      floatingFilter: true,
+      suppressHeaderMenuButton: true,
+      unSortIcon: true,
+    },
+    {
+      headerName: "Email",
+      field: "email",
+      width: 150,
+      filter: "agTextColumnFilter",
+      filterParams: {
+        filterOptions: ["contains"],
+        buttons: ["apply", "reset", "clear"],
+        closeOnApply: true,
+      },
+      floatingFilter: true,
+      suppressHeaderMenuButton: true,
+      unSortIcon: true,
+    },
+    {
+      headerName: "Phone",
+      field: "phone",
+      width: 150,
+      filter: "agTextColumnFilter",
+      floatingFilter: true,
+      suppressHeaderMenuButton: true,
+      filterParams: {
+        buttons: ["apply", "reset", "clear"],
+        closeOnApply: true,
+      },
+      unSortIcon: true,
+    },
+    {
+      headerName: "Country",
+      field: "country",
+      width: 150,
+      filter: "agTextColumnFilter",
+      floatingFilter: true,
+      suppressHeaderMenuButton: true,
+      filterParams: {
+        buttons: ["apply", "reset", "clear"],
+        closeOnApply: true,
+      },
+      unSortIcon: true,
+    },
     {
       headerName: "Actions",
       width: 150,
       cellRenderer: ButtonComp,
       cellRendererParams: (params) => ({ rowid: params.data.id }),
+      sortable: false,
     },
   ];
 
@@ -86,21 +175,13 @@ function Home() {
     }
   }, [colDefs, userpreferencemap]);
 
-  const handlePreferences = () => {
-    if (!showPreferences) {
-      alert("Uncheck the boxes to hide columns");
-      setShowPreferences(true);
-    } else {
-      setShowPreferences(false);
-    }
-  };
-
   const savepreferences = () => {
     localStorage.setItem(
       "userpreferencemap",
       JSON.stringify(userpreferencemap)
     );
-    setShowPreferences((prev) => !prev);
+    // setShowPreferences((prev) => !prev);
+    setPreferencedrawer(false);
   };
 
   const handleOnChangeCheckbox = (field, checked) => {
@@ -114,29 +195,83 @@ function Home() {
     (column) => userpreferencemap[column.field] === true
   );
 
+  const gridref = useRef(); //gives reference to grid, wont change between renders
+
+  const onFilterTextBoxChanged = useCallback(() => {
+    gridref.current.api.setGridOption(
+      //uses the grid's ref to filter data in the grid
+      "quickFilterText",
+      document.getElementById("filter-text-box").value //selects teh input of the filter
+    );
+  }, []);
+
+  const togglepreferencesdrawer = (newstate) => {
+    setPreferencedrawer(newstate); //will open or close depending on true or false of newstate
+  };
+
+  const DrawerList = (
+    <div className="d-flex gap-2 flex-column align-items-left justify-content-center p-3 m-3">
+      {colDefs.map((column) => (
+        <label key={column.headerName.toLowerCase()}>
+          <input
+            type="checkbox"
+            checked={userpreferencemap[column.field]}
+            disabled={column.headerName === "Actions"}
+            onChange={(e) =>
+              handleOnChangeCheckbox(column.field, e.target.checked)
+            }
+          />
+          {column.headerName}
+        </label>
+      ))}
+      <button
+        onClick={savepreferences}
+        className="btn btn-outline-dark mt-3"
+        data-tooltip-id="save-tooltip"
+        data-tooltip-content="Save Preferences"
+        data-tooltip-place="bottom"
+      >
+        Save
+        <Tooltip id="save-tooltip" style={{ fontSize: "12px" }} />
+      </button>
+    </div>
+  );
+
   return (
     <div
-      className="d-flex bg-light m-3 p-2 flex-column border shadow rounded-3 justify-content-between"
+      className="d-flex bg-light m-3 p-3 mt-3 flex-column border shadow rounded-3 justify-content-between"
       style={{ height: "100vh-8px" }}
     >
-      <Link
-        to="/create"
-        className="btn btn-success mb-3 btn-sm"
-        style={{ width: "30px" }}
-        data-tooltip-id="my-tooltip"
-        data-tooltip-content="Add Record"
-        data-tooltip-place="top"
-      >
-        <IoMdAdd />
-        <Tooltip id="my-tooltip" />
-      </Link>
+      <div className="d-flex mt-2 flex-row-reverse" style={{ gap: "941px" }}>
+        <Link
+          to="/create"
+          className="btn btn-outline-dark mb-3 btn-sm"
+          style={{ width: "30px", height: "30px" }}
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Add Record"
+          data-tooltip-place="top"
+        >
+          <IoMdAdd />
+          <Tooltip id="my-tooltip" />
+        </Link>
+        <div className="pb-3 m-lg-1">
+          {/* <span className="m-lg-">Filter: </span> */}
+          <input
+            type="text"
+            id="filter-text-box"
+            placeholder="Filter..."
+            onInput={onFilterTextBoxChanged}
+          />
+        </div>
+      </div>
 
       <div className="d-flex gap-3 justify-content-end">
         <div
           className="ag-theme-quartz"
-          style={{ height: "50vh", width: "89%" }}
+          style={{ height: "55vh", width: "92%" }}
         >
           <AgGridReact
+            ref={gridref}
             rowData={data}
             columnDefs={visibleColDefs}
             onGridReady={onGridReady}
@@ -144,25 +279,25 @@ function Home() {
             onSelectionChanged={(event) => {
               setSelectedRows(event.api.getSelectedRows());
             }}
-            className="w-100 h-100"
+            className="w-100 h-100;"
             pagination={true}
             paginationAutoPageSize={true}
           />
         </div>
         <div className="d-flex flex-column gap-3 ">
-          <Dropdown>
-            <Dropdown.Toggle
-              className="btn btn-sm btn-dark"
-              onClick={handlePreferences}
-              data-tooltip-id="my-tooltip4"
-              data-tooltip-content="Preferences"
-              data-tooltip-place="bottom"
-            >
-              <SlOptionsVertical />
-              <Tooltip id="my-tooltip4" />
-              {/* for hover effect */}
-            </Dropdown.Toggle>
-          </Dropdown>
+          <button
+            onClick={() => togglepreferencesdrawer(true)}
+            className="btn btn-sm btn-outline-dark"
+          >
+            <SlOptionsVertical />
+          </button>
+          <Drawer
+            open={preferencedrawer}
+            onClose={() => togglepreferencesdrawer(false)}
+            anchor="right"
+          >
+            {DrawerList}
+          </Drawer>
           {showPreferences && (
             <div className="d-flex gap-2 flex-column align-items-left justify-content-center">
               {colDefs.map((column) => (
@@ -180,7 +315,7 @@ function Home() {
               ))}
               <button
                 onClick={savepreferences}
-                className="btn btn-dark mt-3"
+                className="btn btn-outline-dark mt-3"
                 data-tooltip-id="save-tooltip"
                 data-tooltip-content="Save Preferences"
                 data-tooltip-place="bottom"
@@ -194,7 +329,10 @@ function Home() {
       </div>
       {isrowselected && (
         <div className="d-flex w-100 h-50 justify-content-center gap-3 pt-3 pb-2">
-          <div className="border bg-white shadow px-5 pt-3 pb-3 rounded" style={{width:"88%"}}>
+          <div
+            className="border bg-white shadow px-5 pt-3 pb-3 rounded"
+            style={{ width: "92%"}}
+          >
             <h3>User Details</h3>
             <div className="mb-2">
               <strong>Name: {selectedRows[0].name}</strong>
@@ -209,12 +347,6 @@ function Home() {
               <strong>Phone: {selectedRows[0].phone}</strong>
             </div>
           </div>
-          {/* <button
-            className="btn btn-lg"
-            onClick={(e) => setisrowselected(false)}
-          >
-            <BiSolidHide />
-          </button> */}
         </div>
       )}
     </div>
